88909585249a4167ce49c02936ec01f80fe7f72c
sk <123456@qq.com>
2026-04-30 09:53:42 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/skeletongo/game-stack
+
+go 1.25.0
---
+++ update_due.sh
@@ -0,0 +1,8 @@
+go get github.com/dobyte/due/v2@main
+go get github.com/dobyte/due/component/http/v2@main
+go get github.com/dobyte/due/locate/redis/v2@main
+go get github.com/dobyte/due/network/ws/v2@main
+go get github.com/dobyte/due/registry/etcd/v2@main
+go get github.com/dobyte/due/transport/grpc/v2@main
+go get github.com/dobyte/due/eventbus/redis/v2@main
+go mod tidy
